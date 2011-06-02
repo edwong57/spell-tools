b3abecb524ae03e70c76265361988203c9cff035
Peter Koppstein <peak@princeton.edu>
2011-06-02 13:40:12 -0400

    runKnnImpute.rb: first assume standard .info file; -v option

--- bin/runKnnImpute.rb
+++ bin/runKnnImpute.rb
@@ -4,42 +4,114 @@
 # See http://creativecommons.org/licenses/by-nc/3.0/
 # Attribution shall include the copyright notice above.
 
+# This is the Troilkatt version as of May 27, 2011 with minor tweaks by peak@princeton.edu 
+
+BN = File.basename($0)
+
+case ARGV[0]
+when "-v", "--verbose"
+  verbose = true
+  ARGV.shift
+else
+  verbose = false
+end
 
 if ARGV.length < 2
-  puts "ARGS:"
-  puts "0 - pcl file to impute"
-  puts "1 - configuration file (often allInfo.txt)"
-  puts "2 - path to KNN impute program"
-  puts "3 - output file"
-  puts "This script will run KNNimpute on the pcl file.  However, KNNImputer"
-  puts "does not perform correctly on raw single channel data, so these data"
-  puts "are first log transformed, then imputed, then exponentiated." 
+  print <<-EOH
+Syntax: #{BN} [-v | --verbose] ARGS
+ARGS:
+   0 - pcl file to impute - pathname or filename
+   1 - info file (aka configuration file - often GSEnnn.info or allInfo.txt)
+   2 - path to KNN impute program
+   3 - output file
+
+Options:
+   -v | --verbose :: verbose (to stderr)
+
+This script will run KNNimpute on the pcl file.  Since KNNImputer
+does not perform correctly on raw single channel data, such data
+are first log transformed, then imputed, then exponentiated. 
+
+If the configuration file is not in "standard format" then the filename should
+have the GDSID as its period-delimited prefix.
+
+Example:
+  #{BN} GSE13219.sfp.pcl GSE13219.sfp.info /usr/local/bin GSE13219.knn.pcl
+
+Version: 2011.06.02
+EOH
   exit 0
 end
 
 
-#Important configuration columns
+#Important configuration column indices (IO=0):
 GDSID_col = 1
 numChan_col = 20
 log_col = 21
 
 #Read the configuration file and locate the needed info
-if ARGV[0].rindex("/") != nil
-  filename = ARGV[0].slice(ARGV[0].rindex("/") + 1, ARGV[0].length)
-else
-  filename = ARGV[0]
-end
-GDSID = filename.slice(0, filename.index("."))
 
-numChan = logged = nil
+# First try: assume the info file is in "standard format"
+
+filename = File.basename(ARGV[0])
+found = standard = numChan = logged = nil
+row=0
+
 IO.foreach(ARGV[1]) do |line|
-	line.chomp!
-	parts = line.split("\t")
-	if parts[GDSID_col] == GDSID
-		numChan = parts[numChan_col].to_i
-		logged = parts[log_col].to_i
-	end
+  row += 1
+  line.chomp!
+  parts = line.split("\t")
+  if row == 1 and parts[GDSID_col] == "DatasetID"
+    if parts[numChan_col] == "#Channels" and parts[log_col] == "logged"
+      standard = 1
+    elsif verbose
+      $stderr.print "parts[GDSID_col]:", parts[GDSID_col], "\n"
+      $stderr.print "parts[numChan_col]:", parts[numChan_col], "\n"
+      $stderr.print "parts[log_col]:", parts[log_col], "\n"
+    end
+  end
+  if row == 2
+    if standard
+      GDSID=parts[GDSID_col]
+      numChan = parts[numChan_col].to_i
+      logged = parts[log_col].to_i
+      found = 1
+    end
+  end
+  break if row > 2
 end
+
+if !found
+# Second try:
+  GDSID = filename.slice(0, filename.index("."))
+  if verbose
+    $stderr.print "Searching for GDSID=#{GDSID} based on filename\n"
+  end
+  IO.foreach(ARGV[1]) do |line|
+    line.chomp!
+    parts = line.split("\t")
+    if parts[GDSID_col] == GDSID
+      found = 2
+      numChan = parts[numChan_col].to_i
+      logged = parts[log_col].to_i
+      break
+    end
+  end
+end
+
+if !found or !numChan or !logged
+  $stderr.print "#{BN}: unable to make sense of info file #{ARGV[1]}:\n"
+  case found
+  when 1
+    $stderr.print "GDSID in info file is #{GDSID}.\n"
+  when 2
+    $stderr.print "GDSID, which was computed from filename as #{GDSID}, was not found in info file.\n"
+  else
+    $stderr.print "GDSID, which was computed from filename as #{GDSID}, was found in info file.\n"
+  end
+  exit 1
+end
+  
 
 inFileName = ARGV[0]
 outFileName = ARGV[3]
@@ -73,12 +145,16 @@ if logged == 0
 	outFileName = ARGV[3] + ".tmp2"
 end
 
-#Run KNNImputer
-cmd = ARGV[2] + "KNNImputer -i " + inFileName + " -o " + outFileName + " -k 10 -m 0.7 -d euclidean"
+# Run KNNImputer
+# Ensure we have just one /
+cmd = ARGV[2]
+cmd="${cmd}/" unless cmd[-1] == "/"[0]
+
+cmd += "KNNImputer -i " + inFileName + " -o " + outFileName + " -k 10 -m 0.7 -d euclidean"
 $stderr.puts cmd
 system cmd
 
-#If this file was log transformed, expoentiate it so that probe averaging isn't affected
+#If this file was log transformed, exponentiate it so that probe averaging isn't affected
 if logged == 0
 	fout = File.open(ARGV[3], "w")
 	IO.foreach(outFileName) do |line|
@@ -104,5 +180,3 @@ if logged == 0
 	#$stderr.puts cmd
 	system cmd
 end
-	
-
